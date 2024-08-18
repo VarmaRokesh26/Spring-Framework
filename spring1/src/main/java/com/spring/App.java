2648aafc1bb1aa15c1a9f8134083dd634712184b
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-18 16:26:27 +0530

    Java Based Configuration

--- spring1/src/main/java/com/spring/App.java
+++ spring1/src/main/java/com/spring/App.java
@@ -1,7 +1,12 @@
 package com.spring;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.spring.config.AppConfig;
+
+// import org.springframework.context.ApplicationContext;
+// import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
@@ -11,13 +16,21 @@ public class App {
     @SuppressWarnings("resource")
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj1 = context.getBean("alien", Alien.class);
-        System.out.println(obj1.getAge());
-        obj1.code();
 
-        Computer computer = context.getBean(Computer.class);
-        computer.compile();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Desktop dt = context.getBean(Desktop.class);
+        dt.compile();
+
+
+
+
+        // ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        // Alien obj1 = context.getBean("alien", Alien.class);
+        // System.out.println(obj1.getAge());
+        // obj1.code();
+
+        // Computer computer = context.getBean(Computer.class);
+        // computer.compile();
         // Desktop desk = context.getBean(Desktop.class);
         // desk.compile();
 
