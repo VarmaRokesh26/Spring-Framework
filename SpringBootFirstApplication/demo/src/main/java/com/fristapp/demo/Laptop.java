7e9dad9ffcb9e6000af32bf969f58226f63456b6
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-18 08:29:52 +0530

    AutoWiring

---
+++ SpringBootFirstApplication/demo/src/main/java/com/fristapp/demo/Laptop.java
@@ -0,0 +1,11 @@
+package com.fristapp.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile() {
+        System.out.println("Compiling Code");
+    }
+}
