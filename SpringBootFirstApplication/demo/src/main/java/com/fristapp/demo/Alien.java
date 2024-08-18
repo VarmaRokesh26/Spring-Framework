2af03894ed18efecf3a83361e79f5e4a98a6542b
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-18 08:21:10 +0530

    Dependency Injection using Spring Boot

--- SpringBootFirstApplication/demo/src/main/java/com/fristapp/demo/Alien.java
+++ SpringBootFirstApplication/demo/src/main/java/com/fristapp/demo/Alien.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 public class Alien {
 
     public void code() {
-        System.out.println("Coding");
+        System.out.println("Coding");  
     }
 }
