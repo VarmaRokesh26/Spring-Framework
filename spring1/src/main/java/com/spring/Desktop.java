c6439c1d6fbe0f968e5508c0bdc347a8537624f8
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-18 15:45:28 +0530

    Lazy Initialized Bean

--- spring1/src/main/java/com/spring/Desktop.java
+++ spring1/src/main/java/com/spring/Desktop.java
@@ -2,6 +2,10 @@ package com.spring;
 
 public class Desktop implements Computer{
 
+    public Desktop() {
+        System.out.println("Desktop Object is Created");
+    }
+    
     @Override
     public void compile() {
         System.out.println("Compiling the program in Desktop");
