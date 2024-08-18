afa78cc3168ba077f010b13aab522b4c0e7523e5
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-18 22:38:56 +0530

    Scope and value Annotation

--- spring1/src/main/java/com/spring/Laptop.java
+++ spring1/src/main/java/com/spring/Laptop.java
@@ -1,10 +1,12 @@
 package com.spring;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Scope("prototype")
 public class Laptop implements Computer{
 
     public Laptop() {
