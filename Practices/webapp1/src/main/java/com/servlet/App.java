5c9f394257b16839db4ae09dbb09204e7bb2cdb6
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-28 01:05:10 +0530

    fresh start

--- Practices/webapp1/src/main/java/com/servlet/App.java
+++ Practices/webapp1/src/main/java/com/servlet/App.java
@@ -1,11 +1,15 @@
-package com.webapp;
+package com.servlet;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
-
-public class App {
-    public static void main( String[] args ) throws LifecycleException {
+import org.apache.catalina.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws LifecycleException
+    {
         System.out.println( "Hello World!" );
         Tomcat tomcat = new Tomcat();
         
---
+++ Practices/webapp1/src/main/java/com/servlet/HelloServlet.java
@@ -0,0 +1,12 @@
+package com.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    public void service(HttpServletRequest req, HttpServletResponse res) {
+        System.out.println("In Service");
+    }
+}
