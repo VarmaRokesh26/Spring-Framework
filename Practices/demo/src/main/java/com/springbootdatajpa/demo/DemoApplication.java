a2d4e8455973eaf61720c5a1160b153afcd685d4
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-27 15:45:56 +0530

    findAll() in JPA

--- Practices/demo/src/main/java/com/springbootdatajpa/demo/DemoApplication.java
+++ Practices/demo/src/main/java/com/springbootdatajpa/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import com.springbootdatajpa.demo.model.Student;
+// import com.springbootdatajpa.demo.model.Student;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -14,29 +14,31 @@ public class DemoApplication {
 
 		StudentRepo repo = context.getBean(StudentRepo.class);
 
-		Student s1 = context.getBean(Student.class);
+		// Student s1 = context.getBean(Student.class);
 
-		s1.setRollNo(101);
-		s1.setName("Shreyas");
-		s1.setMarks(80);
+		// s1.setRollNo(101);
+		// s1.setName("Shreyas");
+		// s1.setMarks(80);
 
-		Student s2 = context.getBean(Student.class);
+		// Student s2 = context.getBean(Student.class);
 
-		s2.setRollNo(37);
-		s2.setName("Guru Dev Singh P B S");
-		s2.setMarks(90);
+		// s2.setRollNo(37);
+		// s2.setName("Guru Dev Singh P B S");
+		// s2.setMarks(90);
 
-		Student s3 = context.getBean(Student.class);
+		// Student s3 = context.getBean(Student.class);
 
-		s3.setRollNo(86);
-		s3.setName("Varma");
-		s3.setMarks(95);
+		// s3.setRollNo(86);
+		// s3.setName("Varma");
+		// s3.setMarks(95);
 
-		// System.out.println("Hello");
+		// // System.out.println("Hello");
 
-		repo.save(s1);
-		repo.save(s2);
-		repo.save(s3);
+		// // repo.save(s1);
+		// repo.save(s2);
+		// repo.save(s3);
+
+		repo.findAll().forEach(System.out::println);
 	}
 
 }
