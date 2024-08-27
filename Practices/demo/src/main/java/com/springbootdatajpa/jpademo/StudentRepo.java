254d55ecbe2c514d6c66c28f5c158f302697c2fd
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-27 22:07:07 +0530

    findByName without Query

--- Practices/demo/src/main/java/com/springbootdatajpa/jpademo/StudentRepo.java
+++ Practices/demo/src/main/java/com/springbootdatajpa/jpademo/StudentRepo.java
@@ -1,7 +1,6 @@
 package com.springbootdatajpa.jpademo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springbootdatajpa.jpademo.model.Student;
@@ -11,6 +10,6 @@ import java.util.List;
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Integer> {
 
-    @Query("select s from Student s where s.name = ?1")
+    // @Query("select s from Student s where s.name = ?1")
     List<Student> findByName(String name);
 }
