2f3cb36252e4be0f6314ed5eb5883156e525cc78
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-28 01:33:33 +0530

    Request and Response in Web

--- Practices/webapp1/src/main/java/com/servlet/HelloServlet.java
+++ Practices/webapp1/src/main/java/com/servlet/HelloServlet.java
@@ -1,12 +1,14 @@
 package com.servlet;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
-    public void service(HttpServletRequest req, HttpServletResponse res) {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("In Service");
+        res.getWriter().println("Hello World");
     }
 }
