a2bfd570b44330837137105b1c58e7611293ccc5
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-31 03:01:00 +0530

    Addition of Twp Numbers using HttpServletRequest, HttpSession, with JSP and JSTL

--- Practices/springbootweb1/src/main/java/com/sbootweb1/springbootweb1/HomeController.java
+++ Practices/springbootweb1/src/main/java/com/sbootweb1/springbootweb1/HomeController.java
@@ -3,11 +3,26 @@ package com.sbootweb1.springbootweb1;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
 
     @RequestMapping
     public String home() {
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req, HttpSession session) {
+
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int num = num1 + num2;
+        session.setAttribute("num", num);
+
+        System.out.println(num);
+        return "result.jsp";
     }
 }
---
+++ Practices/springbootweb1/src/main/webapp/result.jsp
@@ -0,0 +1,11 @@
+<%@page language="java" %>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    </body>
+        <!-- <h2>Addition of Two Numbers : <%= session.getAttribute("num")%></h2> -->
+        <h2>Addition of Two Numbers : ${num}</h2>
+    </body>
+</html>
