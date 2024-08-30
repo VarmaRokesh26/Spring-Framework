aecfdeb5ddee14e57523836729c6b5b4aa4881f9
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-30 09:26:34 +0530

    Initialized project

---
+++ Projects/imsapp/src/test/java/com/ims/imsapp/ImsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ims.imsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
