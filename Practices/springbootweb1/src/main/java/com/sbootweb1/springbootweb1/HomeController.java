808a6058d0d3e2d307e65ad2fe99d56f31a89e68
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-31 16:06:16 +0530

    Preffix and Suffix in applicaion.properties

--- Practices/springbootweb1/src/main/java/com/sbootweb1/springbootweb1/HomeController.java
+++ Practices/springbootweb1/src/main/java/com/sbootweb1/springbootweb1/HomeController.java
@@ -1,26 +1,25 @@
 package com.sbootweb1.springbootweb1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
     @RequestMapping
     public String home() {
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num11, int num2, HttpSession session) {
+    public String add(@RequestParam("num1") int num11, int num2, Model model) {
 
         int num = num11 + num2;
-        session.setAttribute("num", num);
+        model.addAttribute("num", num);
 
         System.out.println(num);
-        return "result.jsp";
+        return "result";
     }
 }
