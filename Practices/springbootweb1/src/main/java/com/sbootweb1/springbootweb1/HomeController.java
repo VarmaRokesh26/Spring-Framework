7429df3f17c8b63817b55f8070d921ce1eafbfa4
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-31 16:13:57 +0530

    ModelAndView

--- Practices/springbootweb1/src/main/java/com/sbootweb1/springbootweb1/HomeController.java
+++ Practices/springbootweb1/src/main/java/com/sbootweb1/springbootweb1/HomeController.java
@@ -1,9 +1,9 @@
 package com.sbootweb1.springbootweb1;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -14,12 +14,13 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num11, int num2, Model model) {
+    public ModelAndView add(@RequestParam("num1") int num11, int num2, ModelAndView mv) {
 
         int num = num11 + num2;
-        model.addAttribute("num", num);
+        mv.addObject("num", num);
+        mv.setViewName("result");
 
         System.out.println(num);
-        return "result";
+        return mv;
     }
 }
