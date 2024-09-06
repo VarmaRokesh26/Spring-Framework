c4dc851e829d8f6ae3e551a504f01e114110fe99
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-09-06 22:28:26 +0530

    Job portal app with React

---
+++ jobapp/src/test/java/com/webapp/jobapp/JobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.jobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
