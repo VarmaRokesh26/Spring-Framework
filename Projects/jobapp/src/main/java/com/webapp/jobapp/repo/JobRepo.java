5406462254d63a8e162f7e7b1062279d16aa0065
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-09-10 15:30:56 +0530

    Reapply "E-commerce store's backend first commit"
    This reverts commit 1b4a235a16cf1c39ddeae76cf30398d18bfd9c6e.

--- Projects/jobapp/src/main/java/com/webapp/jobapp/repo/JobRepo.java
+++ Projects/jobapp/src/main/java/com/webapp/jobapp/repo/JobRepo.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface JobRepo extends JpaRepository<JobPost, Integer>{
 
-	List<JobPost> findByPostProfileContainingOrOrPostDescContainingAllIgnoreCaseOrReqExperience(String postProfile, String postDesc, int reqExperience);
+	List<JobPost> findByPostProfileContainingIgnoringCaseOrPostDescContainingIgnoringCaseOrReqExperienceIs(String postProfile, String postDesc, int reqExperience);
 
 }
