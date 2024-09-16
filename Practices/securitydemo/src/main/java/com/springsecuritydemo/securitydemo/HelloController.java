252900534b1f686ff9d4b92782bf27204567ea56
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-09-16 21:14:31 +0530

    Understanding Session ID for each Login

--- Practices/securitydemo/src/main/java/com/springsecuritydemo/securitydemo/HelloController.java
+++ Practices/securitydemo/src/main/java/com/springsecuritydemo/securitydemo/HelloController.java
@@ -3,12 +3,19 @@ package com.springsecuritydemo.securitydemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class HelloController {
 
     @GetMapping("hello")
-    public String greet() {
-        return "Hello Welcome";
+    public String greet(HttpServletRequest request) {
+        return "Hello Welcome" + request.getSession().getId();
+    }
+
+    @GetMapping("about")
+    public String about(HttpServletRequest request) {
+        return "About us" + request.getSession().getId();
     }
 }
  
