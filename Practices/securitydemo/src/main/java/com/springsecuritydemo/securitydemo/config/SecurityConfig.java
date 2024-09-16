0197b22fcecd34c920fe84ed8d6fce313637fea1
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-09-17 00:41:05 +0530

    without lambda

--- Practices/securitydemo/src/main/java/com/springsecuritydemo/securitydemo/config/SecurityConfig.java
+++ Practices/securitydemo/src/main/java/com/springsecuritydemo/securitydemo/config/SecurityConfig.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+// import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,11 +16,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         
-        http.csrf(customizer -> customizer.disable());
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        // http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> t) {
+                t.disable();
+            }
+        };
+
+        http.csrf(custCsrf);
+
+        // http.csrf(customizer -> customizer.disable());
+        // http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        // // http.formLogin(Customizer.withDefaults());
+        // http.httpBasic(Customizer.withDefaults());
+        // http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        
         return http.build();
     }
 }
