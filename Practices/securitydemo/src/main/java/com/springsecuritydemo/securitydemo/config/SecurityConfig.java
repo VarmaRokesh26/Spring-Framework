1ddb182df5cce85e4ae42f6643a652c7cdcd20c3
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-09-17 00:31:15 +0530

    Disabling CSRF using Lambda Expression

--- Practices/securitydemo/src/main/java/com/springsecuritydemo/securitydemo/config/SecurityConfig.java
+++ Practices/securitydemo/src/main/java/com/springsecuritydemo/securitydemo/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.springsecuritydemo.securitydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,6 +15,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        // http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 }
