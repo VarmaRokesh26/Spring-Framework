edef6dc77431c8f5b473e2a58769c3c48ac374f2
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-09-17 13:54:54 +0530

    Authentication after encoding

--- Practices/securitydemo/src/main/java/com/springsecuritydemo/securitydemo/config/SecurityConfig.java
+++ Practices/securitydemo/src/main/java/com/springsecuritydemo/securitydemo/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 // import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 // import org.springframework.security.core.userdetails.User;
 // import org.springframework.security.core.userdetails.UserDetails;
 // import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,12 +25,11 @@ public class SecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
 
-    @SuppressWarnings("deprecation")
     @Bean
     public AuthenticationProvider authProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         return provider;
     }
 
