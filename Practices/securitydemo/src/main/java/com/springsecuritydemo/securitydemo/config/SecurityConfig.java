2b28599b5a40434d7fd3e5c0887890f1746065d0
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-09-18 13:57:19 +0530

    Custom Login

--- Practices/securitydemo/src/main/java/com/springsecuritydemo/securitydemo/config/SecurityConfig.java
+++ Practices/securitydemo/src/main/java/com/springsecuritydemo/securitydemo/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.springsecuritydemo.securitydemo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 // import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -47,7 +49,7 @@ public class SecurityConfig {
 
         http.csrf(customizer -> customizer.disable())
             .authorizeHttpRequests(request -> request
-                .requestMatchers("register")
+                .requestMatchers("register", "login")
                 .permitAll()
                 .anyRequest().authenticated())
             .httpBasic(Customizer.withDefaults())
@@ -78,4 +80,9 @@ public class SecurityConfig {
 
     //     return new InMemoryUserDetailsManager(user, admin);   
     // }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
--- Practices/securitydemo/src/main/java/com/springsecuritydemo/securitydemo/controller/UserController.java
+++ Practices/securitydemo/src/main/java/com/springsecuritydemo/securitydemo/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.springsecuritydemo.securitydemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +18,22 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+
     @PostMapping("register")
     public User register(@RequestBody User user) {
         userService.saveUser(user);
         return user;
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user) {
+        Authentication authentication = authenticationManager
+                                        .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated()) return "SUCCESS";
+        else return "LOGIN FAILED";
     }
 }
